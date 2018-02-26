ba5196880ac9b5e083dbf74aa8318bd216db2b69
imprezka88 <imprezka88@gmail.com>
2018-02-26 12:58:59 -0500

    [IGOWEB-1031] Add more verbose logging for no valid samples

--- src/main/java/org/mskcc/domain/sample/ValidSamplePredicate.java
+++ src/main/java/org/mskcc/domain/sample/ValidSamplePredicate.java
@@ -1,11 +1,19 @@
 package org.mskcc.domain.sample;
 
+import org.apache.log4j.Logger;
+
 import java.util.function.Predicate;
 
 class ValidSamplePredicate implements Predicate<Sample> {
+    private static final Logger LOGGER = Logger.getLogger(ValidSamplePredicate.class);
+
     @Override
     public boolean test(Sample sample) {
-        return sample.getRuns().values().stream()
+        boolean hasValidRun = sample.getRuns().values().stream()
                 .anyMatch(r -> r.isPassed());
+
+        LOGGER.warn(String.format("Sample %s has no valid runs. It will be omitted.", sample.getIgoId()));
+
+        return hasValidRun;
     }
 }
